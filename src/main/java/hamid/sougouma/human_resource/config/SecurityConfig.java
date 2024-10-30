58b998514ccc03c0e0a01e0bcf6c7dc26d437073
Sougouma-Ali HAMID <sougouma-ali.hamid@apside.com>
2024-10-30 13:25:31 +0100

    mise en place de l'authentification avec keycloak

--- src/main/java/hamid/sougouma/human_resource/config/SecurityConfig.java
+++ src/main/java/hamid/sougouma/human_resource/config/SecurityConfig.java
@@ -1,8 +1,59 @@
 package hamid.sougouma.human_resource.config;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-    
+
+    public static final String ADMIN = "hr-admin";
+    public static final String USER = "hr-user";
+
+    private final JwtConverter jwtAuthConverter;
+
+    public SecurityConfig(JwtConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
+
+//    @Value("spring.security.oauth2.resourceserver.jwt.issuer-uri")
+//    private String issuer;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.GET, "/api/hello").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/admin").hasRole(ADMIN)
+                        .requestMatchers(HttpMethod.GET, "/api/user").hasRole(USER)
+                        .requestMatchers( "/users/*").hasAnyRole(ADMIN,USER)
+                        .anyRequest().authenticated()
+
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+//    @Bean
+//    public JwtDecoder jwtDecoder() {
+//        return JwtDecoders.fromIssuerLocation("http://localhost:8080/realms/human_resource_keycloack");
+//    }
 }
